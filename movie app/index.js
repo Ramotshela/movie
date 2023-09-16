cbdcb898b1523351018fd8d497ebbe5a52c385ff
samuel ramotshela <122275648+Ramotshela@users.noreply.github.com>
2023-09-16 02:24:48 +0200

    Add files via upload

---
+++ movie app/index.js
@@ -0,0 +1,3 @@
+const APILINK="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0d11755b34a665f26c4fa2a921415265&page=1"
+const path_Img='https://image.tmdb.org/t/p/w1280'
+const searchApi='https://api.themoviedb.org/3/search/movie?&api_key=0d11755b34a665f26c4fa2a921415265&query='
